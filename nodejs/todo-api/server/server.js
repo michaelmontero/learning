bb7572f65a50de91feb09b1ac9fe564003e9cbc9
Michael Montero <monterom334@gmail.com>
2018-07-26 14:07:26 -0400

    Add todo

--- nodejs/todo-api/server/server.js
+++ nodejs/todo-api/server/server.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 7979;
+
 var {mongose} = require("./db/mongoose");
+var { Todo } = require("./models/Todo");
+var { User } = require("./models/User");
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post("/todo", (req, res)=>{
+  var todo = new Todo({
+    text : req.body.text
+  });
+
+  todo.save().then((doc)=>{
+    res.json(doc);
+  }, (e)=>{
+    res.status(400).send(e);
+  });
+
+});
+
+app.listen(port, ()=>{
+  console.log(`Serve started on port ${port}`)
+})
