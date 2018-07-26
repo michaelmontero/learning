a3bc4080d2d8fffe90b9a53d4776929307d0f4fa
Michael Montero <monterom334@gmail.com>
2018-07-26 14:54:07 -0400

    Refactoring

--- nodejs/todo-api/server/server.js
+++ nodejs/todo-api/server/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 7979;
 
-var {mongose} = require("./db/mongoose");
 var { Todo } = require("./models/Todo");
 var { User } = require("./models/User");
+var { TodoService } = require("./services/todo.services");
 
 var app = express();
 app.use(bodyParser.json());
@@ -13,13 +13,7 @@ app.post("/todo", (req, res)=>{
   var todo = new Todo({
     text : req.body.text
   });
-
-  todo.save().then((doc)=>{
-    res.json(doc);
-  }, (e)=>{
-    res.status(400).send(e);
-  });
-
+  res.json(TodoService.add(todo));
 });
 
 app.listen(port, ()=>{
