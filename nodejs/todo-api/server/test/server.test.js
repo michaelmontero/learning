cbcabafa275e6fc2be744fef9995eb07882b7085
Michael Montero <monterom334@gmail.com>
2018-07-27 14:53:56 -0400

    Test post todo method

--- nodejs/todo-api/server/test/server.test.js
+++ nodejs/todo-api/server/test/server.test.js
@@ -4,6 +4,26 @@ const request = require("supertest");
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
-// describe("POST /todos", ()=>{
-//
-// })
+describe("POST /todos", ()=>{
+  it("It should create a new todo", (done) =>{
+    var text = "Testing..";
+
+    request(app)
+      .post("/todo")
+      .send({text});
+      .expect(200)
+      .expect((res) =>{
+          expect(res.body.text).toBe(text);
+      })
+      .end((err, res)=>{
+        if(err){
+          return done(err);
+        }
+        Todo.find().then((todos)=>{
+          expect(todo.length).toBe(1);
+          expect(todo[0].text).toBe(text);
+          done();
+        }).catch((e)=> done(e));
+      });
+  });
+})
