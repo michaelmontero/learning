19821c7b61a0af8647bb1b68cf3febb15db17919
Michael Montero <monterom334@gmail.com>
2018-07-27 11:52:16 -0400

    Refactoring

--- nodejs/todo-api/server/server.js
+++ nodejs/todo-api/server/server.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const port = process.env.PORT || 7979;
+var {mongose} = require("./db/mongoose");
 
 var { Todo } = require("./models/Todo");
 var { User } = require("./models/User");
 var { TodoService } = require("./services/todo.services");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 7979;
 
 var app = express();
 app.use(bodyParser.json());
@@ -13,7 +15,13 @@ app.post("/todo", (req, res)=>{
   var todo = new Todo({
     text : req.body.text
   });
-  res.json(TodoService.add(todo));
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+      res.status(400).send(e);
+  });
+
 });
 
 app.listen(port, ()=>{
--- nodejs/todo-api/server/services/todo.services.js
+++ nodejs/todo-api/server/services/todo.services.js
@@ -1,14 +1,7 @@
-var {mongose} = require("./../db/mongoose");
 
-var { Todo } = require("./../models/Todo");
-var { User } = require("./../models/User");
 
 var add = (todo)=>{
-  todo.save().then((doc)=>{
-    return doc;
-  },(e)=>{
-      return {e};
-  });
+  
 };
 
 module.exports = {
