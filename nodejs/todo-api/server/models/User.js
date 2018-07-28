89188cae98b87ce59a2e5c3a95ccd4bfb25d0249
Michael Montero <monterom334@gmail.com>
2018-07-27 23:14:39 -0400

    Update User model. Create post user service

--- nodejs/todo-api/server/models/User.js
+++ nodejs/todo-api/server/models/User.js
@@ -1,12 +1,33 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 var User = mongoose.model("User",  {
   email :  {
-  required : true,
-  type : String,
-  trim : true,
-  minLength : 4
-  }
+    required : true,
+    type : String,
+    trim : true,
+    minLength : 4,
+    unique : true,
+    validate :{
+      validator : validator.isEmail,
+      message : `{VALUE } is not a valid email`
+    }
+  },
+  password :{
+    type : String,
+    required : true,
+    minLength : 6
+  },
+  token : [{
+    access :{
+      type : String,
+      require : true
+    },
+    token : {
+      type : String,
+      require : true
+    }
+  }]  
 });
 
 module.exports = {User};
--- nodejs/todo-api/server/server.js
+++ nodejs/todo-api/server/server.js
@@ -1,12 +1,15 @@
 require("./config/config.js");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
 var {mongose} = require("./db/mongoose");
 var {ObjectId} = require("mongodb");
 var { Todo } = require("./models/Todo");
 var { User } = require("./models/User");
 
 
-const express = require("express");
-const bodyParser = require("body-parser");
+
 const port = process.env.PORT || 7979;
 
 var app = express();
@@ -45,6 +48,17 @@ app.post("/todo", (req, res)=>{
       res.status(400).send(e);
   });
 
+});
+
+app.post("/user", (req, res)=>{
+  var body = _.pick(req.body, ["email", "password"])
+  console.log("body", body);
+  var user = new User(body);
+  user.save().then((user)=>{
+    res.send(user);
+  }).catch((err)=>{
+    res.status(400).send(err);
+  });
 });
 
 app.listen(port, ()=>{
