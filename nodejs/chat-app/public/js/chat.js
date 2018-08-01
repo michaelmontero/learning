50d8f35ac8b2e57136917b5954369ac9311f5d2d
Michael Montero <monterom334@gmail.com>
2018-08-01 12:16:39 -0400

    Lister and respond to some events

---
+++ nodejs/chat-app/public/js/chat.js
@@ -0,0 +1,21 @@
+(function(){
+    var socket = io();
+
+    socket.on("connect", function(){
+        console.log("Connected to server");
+    });
+
+    socket.on("newMessage", function(data){
+        console.log("New message", JSON.stringify(data, undefined, 2));
+    });
+
+    socket.emit("createMessage", {
+        "to" : "monterom334@gmail.com",
+        "text" : "Some message",
+        "subject" : "Learning"
+    });
+
+    socket.on("disconnect", function(){
+        console.log("Disconected from server.");
+    });
+}());
--- nodejs/chat-app/server/server.js
+++ nodejs/chat-app/server/server.js
@@ -1,12 +1,37 @@
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 const path = require("path");
 
 const port = process.env.PORT || 3000;
 const partialPath = path.join(__dirname,'../public');
 const app = express();
 
+const server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(partialPath));
 
-app.listen(port, ()=>{
+io.on("connection", (socket)=>{
+    socket.emit("newEmail", {
+        "from" : "monterom334@gmail.com",
+        "text" : "Some message",
+        "subject" : "Learning"
+    });
+
+    socket.on("createMessage",(message)=>{
+        console.log("New message is comming", JSON.stringify(message, undefined, 2));
+    });
+
+    socket.on("newMessage",(message)=>{
+        console.log(JSON.stringify(message, undefined, 2));
+    });
+
+    io.on("disconnect",(socket)=>{
+        console.log("Disconected");
+    });
+});
+
+server.listen(port, ()=>{
     console.log(`App running on port ${port}`);
 });
